05cb916b60dff7324ed6c84e56aa83d7bcbef7d2
SiMan <951400676@qq.com>
2023-07-23 23:34:07 +0800

    feature: 开发 HTTP 接口幂等 Demo

---
+++ speraph-demo/src/main/java/com/nageoffer/speraph/demo/SperaphDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nageoffer.speraph.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SperaphDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SperaphDemoApplication.class, args);
+    }
+
+}
